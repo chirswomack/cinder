b84560c404909cafcaf3c89dc02372659d08ffd4
Raymond Wu <raywu22@mit.edu>
2015-09-20 06:44:20 -0400

    buncha stuff

--- cinderServer/routes/index.js
+++ cinderServer/routes/index.js
@@ -71,4 +71,120 @@ router.post("/login",function(req,res,next){
 		});
 	});
 });
+
+router.post("/updateProfileApplicant",function(req,res,next){
+	applicantModel.update({username:req.body.username
+							},{fullName:req.body.fullName,
+								email:req.body.email,
+								age:req.body.age,
+								academics:req.body.academics,
+								jobHistory:req.body.jobHistory,
+								location:req.body.location,
+								bio:req.body.bio,
+								positionType:req.body.positionType,
+								industry:req.body.industry
+							},function(error){
+								res.send("updateProfileApplicantSuccess")
+							});
+});
+
+router.post("/updateProfileCompany",function(req,res,next){
+	employerModel.update({username:req.body.username
+							},{
+								companyName:req.body.companyName,
+								industry:req.body.industry,
+								description:req.body.description,
+								location:req.body.location,
+								email:req.body.email,
+								positionType:req.body.positionType
+						},function(error){
+							res.send("updateProfileCompanySuccess");
+						});
+});
+
+router.post("/dashboardInfo",function(req,res,next){
+	if (req.body.userType==="applicant"){
+		applicantModel.findOne({username:req.body.user},function(error,applicantData){
+			employerModel.findOne({industry:applicantData.industry,
+									positionType:applicantData.positionType
+								}).nin("username",applicantData.companiesRejected)
+									.nin("username",applicantData.companiesAccepted)
+									.exec(function(error,userData){
+				if (userData){
+					var employerInfo = {companyName:userData.companyName,
+										industry:userData.companyName,
+										description:userData.description,
+										location:userData["location"],
+										positionType:userData.positionType
+										}
+					res.send(employerInfo);
+				}
+				else {
+					res.send("noMoreMatches");
+				}
+			});
+		});
+	}
+	else {
+		employerModel.findOne({username:req.body.username},function(error,employerData){
+			applicantModel.findOne({industry:employerData,
+									positionType:{$in:employerData.positionType}
+								}).nin("username",employerData.applicantsAccepted)
+									.nin("username",employerData.applicantsRejected)
+									.exec(function(error,userData){
+				if (userData){
+					var applicantInfo = {academic:userData.academic,
+											jobHistory:userData.jobHistory,
+											location:userData["location"],
+											bio:userData.bio,
+											positionType:userData.positionType,
+											industry:userData.industry
+										}
+					res.send(applicantInfo);
+				}
+				else {
+					res.send("noMoreMatches");
+				}
+			});
+		});
+	}
+});
+
+router.post("/dashboardSwipeLeft",function(req,res,next){
+	if (req.body.userType==="applicant"){
+		applicantModel.findOneAndUpdate({username:req.body.username
+											},{$push:{companiesRejected:req.body.matchName}
+											},function(error){
+												res.send("swipeLeftSuccess");
+											});
+	}
+	else {
+		employerModel.findOneAndUpdate({username:req.body.username},
+										{$push:{applicantsRejected:req.body.matchName}},
+										function(error){
+											res.send("swipeLeftSuccess");
+										});
+	}
+});
+
+router.post("/dashboardSwipeRight",function(req,res,next){
+	if (req.body.userType==="applicant"){
+		applicantModel.findOneAndUpdate({username:req.body.username},{
+			$push:{companiesAccepted:req.body.matchName}
+		},function(error,applicantData){
+			applicantModel.findOne({username:matchName},function(error,matchData){
+				if (matchData.companiesAccepted.indexOf(req.body.username)>-1){
+					res.send("emailsSent");
+				}
+				else {
+					res.send("noEmailSent")
+				}
+			});
+		});
+	}
+	else {
+		
+	}
+})
+
 module.exports = router;
